37db9babc6b4489bd1b46d715973be4e2d0f0b6b
曽和修平 <sowashuhei@gmail.com>
2015-04-20 01:57:16 +0900

    擬似コードで実装（コンパイルできないです）

---
+++ Othello/simpleStrategies/AI.java
@@ -0,0 +1,9 @@
+public abstract class AI {
+	abstract public void move(int[][] borad);
+	
+	public int presearch_depth = 3;
+	public int normal_depth = 15;
+	public int wld_depth = 15;
+	public int perfect_depth = 13;
+
+}
